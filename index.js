179b4dae5eb1bd2d92ba642c398e3bb2ee24a6ad
Zichen Liu <liuzc@hotmail.co.uk>
2016-11-21 21:51:53 +0000

    NEW REVELATIONS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
  
 app.get('/', function(req, res) {
     var ds = [];
-    for(var i=0; i< 10000; i++){
+    for(var i=0; i< 1000; i++){
         ds[i] = {
             label: 'abcd',
             data: [{
