034f8649ca9a6bea390e33663f87f06b469e3c34
Zichen Liu <liuzc@hotmail.co.uk>
2016-11-29 09:53:13 +0000

    new one

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 var DATASET = [];
+var hSet = new Set();
+var MAX_HASH = 20;
 
 
 app.post('/', function(req, res) {
     //console.log(req.body.origin);
-
     ds = formDataSets(req.body.origin);
 
     res.header('Access-Control-Allow-Origin', '*');
@@ -51,27 +52,61 @@ function formDataSets(origin){
 var SERVER_ADDRESS = "https://oblong-adventures.herokuapp.com";
 function updatePrices() {
 
-    var i = 0;
-    setInterval(function(){
-        addDataSetGroup('#' + Math.floor(Math.random() * 16777215).toString(16), 5, 5, i);
-        i++;
-    }, 100);
-    /*for(var i=990; i<1000; i++){
+    //displays all datapoints up to MAX_PT
+    //var MAX_PT = 1000;
+    /*for(var i=1; i<MAX_PT; i++){
         addDataSetGroup('#' + Math.floor(Math.random() * 16777215).toString(16), 5, 5, i);
     }*/
-    /*for(var i=0; i< parseInt(1000); i++){
-        DATASET[i] = {
-            label: (1000*Math.random()).toString(),
-            x:10*Math.random(),
-            y:10*Math.random(),
-            r:5*Math.random()+8,
-            backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16)
-        };
-    }*/
+
+    var startIdx = 24;
+    addDataSetGroupByLinkReturnInterest('#00FF00', 10, 10, '/api/people/'+startIdx);
+
 }
 
-function addDataSetGroup(dotColor, xOrigin, yOrigin, i){
-    postRequest.get(SERVER_ADDRESS + '/api/people/' + i, function (err, response, body) {
+
+function addDataSetGroupByLinkReturnInterest(dotColor, xOrigin, yOrigin, link){
+    postRequest.get(SERVER_ADDRESS + link, function (err, response, body) {
+        try {
+            var parBody = JSON.parse(body);
+            console.log(link);
+            var keywords = parBody.keywords;
+
+            for(var keys in keywords){
+                getPeopleOfSimilarInterests(keys);
+            }
+
+            addDataSetGroupByHash(dotColor, xOrigin, yOrigin);
+        }catch(e){
+            console.log(link + ' failed ' + e);
+        }
+    });
+}
+
+function getPeopleOfSimilarInterests(topicKeyword){
+    console.log(topicKeyword);
+    postRequest.get(SERVER_ADDRESS + '/api/keywords/' + topicKeyword, function (err, response, body) {
+        try {
+            var parBody = JSON.parse(body);
+            var t = 0;
+            while(parBody.profiles[t]!=null && hSet.size<30) {
+                hSet.add(parBody.profiles[t].link);
+                t++;
+            }
+        }catch(e){console.log(e);}
+    });
+}
+
+
+function addDataSetGroupByHash(dotColor, xOrigin, yOrigin){
+    var i = 0;
+    for(var link in hSet){
+        addDataSetGroupWithLink(dotColor, xOrigin, yOrigin, link, i);
+        i++;
+    }
+}
+
+function addDataSetGroupWithLink(dotColor, xOrigin, yOrigin, link, i){
+    postRequest.get(SERVER_ADDRESS + link, function (err, response, body) {
         try {
             var parBody = JSON.parse(body);
             var name = parBody.name.title + ' ' + parBody.name.first
@@ -92,6 +127,8 @@ function addDataSetGroup(dotColor, xOrigin, yOrigin, i){
     });
 }
 
+
+
 updatePrices();
 
 
@@ -103,3 +140,27 @@ var server = app.listen(PORT, function(){
 
     console.log('Started Server at %s:%s', host, port);
 });
+
+
+
+/*function addDataSetGroup(dotColor, xOrigin, yOrigin, i){
+    postRequest.get(SERVER_ADDRESS + '/api/people/' + i, function (err, response, body) {
+        try {
+            var parBody = JSON.parse(body);
+            var name = parBody.name.title + ' ' + parBody.name.first
+                + ' ' + parBody.name.initials + ' ' + parBody.name.last;
+            var department = parBody.department;
+            var label = ' [' + department + '] ' + name;
+            DATASET[i] = {
+                label: label,
+                x: xOrigin + 3*Math.random(),
+                y: yOrigin + 3*Math.random(),
+                r: 5 * Math.random() + 8,
+                backgroundColor: dotColor
+            };
+            console.log(i);
+        }catch(e){
+            console.log(i + ' failed ' + e);
+        }
+    });
+}*/
