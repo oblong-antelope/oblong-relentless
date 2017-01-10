3b8c72243a461fce77ed8adea195441f582eb746
Zichen Liu <liuzc@hotmail.co.uk>
2017-01-10 11:19:31 +0000

    a

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ function addDataSetGroupByLinkReturnInterest(link){
 
             for(var keys in keywords){
                 //we will assume our data is randomly drawn from an Exp(7) distribution
-                //Warning: these values are not necessarily bounded
+                //Warning: these values are not necessarily boundeda
                 if(keywords[keys]>Math.random()*10) {
                     getPeopleOfSimilarInterests(keys);
                 }
